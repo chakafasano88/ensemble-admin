73104d0c79b35796da4a20425a02839fb7684c02
Matt Fasano <matt.fasano88@gmail.com>
2018-05-29 12:12:33 -0400

    added react-strap and bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { login } from "./actions/auth"
 import { ConnectedRouter } from "react-router-redux";
 import { history } from "./store";
 
+
 class App extends Component {
 
   componentDidMount() {
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from "./store";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 let { store, persistor } = configureStore()
 
